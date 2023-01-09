d72940f4e736b6362a6bbf05a882ff0bdb02489e
karl wernsman <wernsmank@gmail.com>
2023-01-09 14:38:24 -0800

    images, name, and says rendering

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import './Main.css';
 import background from '../../background.png';
+import animals from '../../data.js';
+import Animal from '../Animal/Animal.js';
 
 export default function Main() {
-  return <main style={{ backgroundImage: `url(${background})` }}></main>;
+  return (
+    <main style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((animal) => (
+        <Animal key={animal.name} image={animal.type} says={animal.says} name={animal.name} />
+      ))}
+    </main>
+  );
 }
